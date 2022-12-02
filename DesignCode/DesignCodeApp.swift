bf7c09435488e4d389e59488a286a2dd6a61510d
zhouchaojiang <zhouchaojiang@ofworld.com>
2022-12-02 22:28:20 +0800

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by 周朝江 on 2022/12/2.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
