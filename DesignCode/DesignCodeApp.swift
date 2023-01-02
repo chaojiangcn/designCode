cf5966f15436bc85b798048cf83dcf51a59f0083
zhouchaojiang <zhouchaojiang@ofworld.com>
2023-01-02 21:13:02 +0800

    first commit

--- DesignCode/DesignCodeApp.swift
+++ DesignCode/DesignCodeApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct DesignCodeApp: App {
+    @StateObject var model = Model() // Avoid calling multiple times, ensures that model initilize once and follows the lifecycle of the app
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(model)
         }
     }
 }
